d2b5d7da04dae452f1efc6efe020e24385eddcf1
RUKON2082 <miarukon985@gmail.com>
2024-03-16 21:32:36 +0600

    alpa class pro

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,10 @@
+// function play () {
+//    const  homeSection = document.getElementById('home-screen');
+//    homeSection.classList.add('hidden');
+//    // console.log(homeSection.classList);
+
+//    const playgroundSection = document.getElementById('play-ground');
+//    playgroundSection.classList.remove('hidden');
+//    // console.log(playgroundSection.classList);
+// }
+
